f550339e3c49e430f795ad57f402fb58f3ed8738
wangj38 <wangj38@mingyuanyun.com>
2020-09-09 11:31:55 +0800

    云问用例编写

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ print(sys.path)
 sys.path.insert(0, '.')
 
 if __name__ == '__main__':
-
-    pytest.main(['-s','-v','-m','invest','--clean-alluredir','--html=Outputs/reports/report.html','--alluredir=Outputs/reports'])
+    pytest.main(['-s','-v','--clean-alluredir','--html=Outputs/reports/report.html','--alluredir=Outputs/reports'])
 
 
 
